6d1bd7796ce699f108feb0046e436931c01bf6d8
ayeshaazeema <ayesha.azeema@gmail.com>
2022-02-24 14:00:19 +0700

    Fourth Commit

---
+++ app/src/main/java/com/ayeshaazeema/paw/OnboardingActivity.kt
@@ -0,0 +1,129 @@
+package com.ayeshaazeema.paw
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.viewpager.widget.ViewPager
+import com.ayeshaazeema.paw.adapter.SliderAdapter
+import com.ayeshaazeema.paw.db.AppPreferences
+import kotlinx.android.synthetic.main.activity_on_boarding.*
+
+class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var sliderAdapter: SliderAdapter
+    private var points: Array<TextView?>? = null
+    private lateinit var layouts: Array<Int>
+    private val sliderChangeListener = object : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(state: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onPageScrolled(
+            position: Int,
+            positionOffset: Float,
+            positionOffsetPixels: Int
+        ) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onPageSelected(position: Int) {
+            addBottomPoint(position)
+
+            if (position == layouts.size.minus(1)) {
+                tvSkip.show()
+                tvStart.show()
+                tvNext.hide()
+
+            } else {
+                tvSkip.show()
+                tvNext.show()
+                tvStart.hide()
+
+            }
+        }
+
+    }
+
+    private fun addBottomPoint(lastPosition: Int) {
+        points = arrayOfNulls(layouts.size)
+
+        llDot.removeAllViews()
+        for (i in 0 until points!!.size) {
+            points!![i] = TextView(this)
+            points!![i]?.text = getColor(R.color.yellow).toString()
+            points!![i]?.textSize = 35f
+            points!![i]?.setTextColor(resources.getColor(R.color.yellow_transparent))
+            llDot.addView(points!![i])
+        }
+        if (points!!.isNotEmpty()) {
+            points!![lastPosition]?.setTextColor(resources.getColor(R.color.yellow))
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+        init()
+        dataSet()
+        interactions()
+    }
+
+    private fun interactions() {
+        tvSkip.setOnClickListener {
+            navigateToLogin()
+        }
+        tvStart.setOnClickListener {
+            navigateToLogin()
+        }
+        tvNext.setOnClickListener {
+            val currentPage = getCurrentScreen(+1)
+            if (currentPage < layouts.size) {
+                vpSlider.currentItem = currentPage
+
+            } else {
+                navigateToLogin()
+            }
+        }
+    }
+
+    private fun getCurrentScreen(i: Int): Int = vpSlider
+        .currentItem.plus(i)
+
+    private fun navigateToLogin() {
+        AppPreferences(this).setFirstLaunch(false)
+        startActivity(
+            Intent(
+                this,
+                SignInActivity::class.java
+            )
+        )
+        finish()
+    }
+
+    private fun dataSet() {
+        addBottomPoint(0)
+        vpSlider.apply {
+            adapter = sliderAdapter
+            addOnPageChangeListener(sliderChangeListener)
+        }
+    }
+
+    private fun init() {
+        layouts = arrayOf(
+            R.layout.first_on_boarding,
+            R.layout.second_on_boarding,
+            R.layout.third_on_boarding
+        )
+        sliderAdapter = SliderAdapter(this, layouts)
+    }
+}
+
+private fun View.show() {
+    visibility = View.VISIBLE
+}
+
+private fun View.hide() {
+    visibility = View.GONE
+}
---
+++ app/src/main/java/com/ayeshaazeema/paw/db/AppPreferences.kt
@@ -0,0 +1,27 @@
+package com.ayeshaazeema.paw.db
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class AppPreferences(context: Context) {
+    private lateinit var prefs: SharedPreferences
+    private lateinit var prefsEditor: SharedPreferences.Editor
+
+    init {
+        prefs = context.getSharedPreferences(
+            PREF_NAME,
+            PRIVATE_MODE
+        )
+        prefsEditor = prefs.edit()
+    }
+    companion object{
+        private const val PRIVATE_MODE = 0
+        private const val PREF_NAME = "app-prefs"
+        private const val IS_FIRST_LAUNCH = "IsFirstLaunch"
+    }
+    fun setFirstLaunch(isFirstLaunch: Boolean){
+        prefsEditor.putBoolean(IS_FIRST_LAUNCH, isFirstLaunch)
+        prefsEditor.commit()
+    }
+    fun isFirstLaunch(): Boolean = prefs.getBoolean(IS_FIRST_LAUNCH, true)
+}
